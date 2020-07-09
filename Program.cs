db77848f999773cdaa7f246233e71b9f96752198
Jade <jade.nguyen@zoopla.co.uk>
2020-07-09 13:28:42 +0100

    Hello Everyone practice

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Support_Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
         }
     }
 }
