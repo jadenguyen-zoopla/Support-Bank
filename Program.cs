9d81fc7c9cb4fb2ba14ee534d3197675b80bdfed
Jade <jade.nguyen@zoopla.co.uk>
2020-07-10 10:56:21 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace Support_Bank
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // creating a variable called transactions that runs the ReadTransactionsFromFile() function created on the FileReader file
             var transactions = FileReader.ReadTransactionsFromFile();
+            // print the results found that is now stored in the transaction variable
             Console.WriteLine(transactions);
         }
     }
